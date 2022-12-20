63c98b0a1a39b3322afe1346a65cee2fb7f5baf7
Kazuki624 <audi_kazuki0624@yahoo.co.jp>
2022-12-20 21:24:16 +0900

    make logo

--- src/components/nav/Navbar.jsx
+++ src/components/nav/Navbar.jsx
@@ -3,16 +3,17 @@ import { SearchBar } from "./SearchBar" ;
 import { CartButton } from "./CartButton";
 import { useNavigate,useLocation } from "react-router-dom";
 import { Condition } from "../extra/Condition";
+import logo from "../../img/logo.png"
 
-export const Navbar = ({name}) => {
+export const Navbar = () => {
     const nav = useNavigate()
     const {pathname} = useLocation()
     
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top border-bottom">
             <div className="container-fluid px-5">
-                <span  onClick={() => nav("/")} className="navbar-brand fw-bold pointer " id="name">
-                    {name}
+                <span  onClick={() => nav("/")} className="navbar-brand fw-bold pointer w-25" id="name">
+                    <img src={logo} alt="logo" className="img-fluid"/>
                 </span>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSuppportedContent">
                     <span className="navbar-toggler-icon"></span>
